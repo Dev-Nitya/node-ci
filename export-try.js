81580ad2c070b4b785ab2f58c46b7d3b122ad46d
niya <nitya.devstory@gmail.com>
2020-08-13 14:48:54 +0530

    exports & module.exports

---
+++ export-try.js
@@ -0,0 +1,46 @@
+// const getName = () => {
+// 	return 'Jim';
+// };
+
+// exports.getName = getName;
+
+
+// class User {
+// 	constructor(name, age, email) {
+// 		this.name = name;
+// 		this.age = age;
+// 		this.email = email;
+// 	}
+
+// 	getUserStats(){
+// 		return `
+// 			Name : ${this.name},
+// 			Age  : ${this.age},
+// 			Email : ${this.email}
+// 		`;
+// 	}
+// }
+
+// module.exports = User;
+
+
+
+// module.exports = {
+// 	getName : () => {
+// 		return 'Nitya';
+// 	},
+
+// 	getAge : () => {
+// 		return 25;
+// 	}
+// }
+
+
+
+exports.getName = () => {
+	return 'Nitya';
+}
+
+module.exports = () => {
+	return 25;
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+// const User = require('./export-try');
+
+// const newUser = new User('Nitya', 25 , 'malhotra_nitya@ymail.com');
+
+// console.log(newUser.getUserStats());
+
+
+const User = require('./export-try');
+console.log(User);
+
+// console.log(getName());
+// console.log(getAge());
