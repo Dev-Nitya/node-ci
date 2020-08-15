6b38ffdd377721adea46b20ac360d0f5da2fa389
niya <nitya.devstory@gmail.com>
2020-08-15 18:04:46 +0530

    Async/await

--- es8-try.js
+++ es8-try.js
@@ -12,5 +12,39 @@ let entriesArr = Object.entries(names);
 
 //console.log(Object.values(names));
 
-let newString = 'loading'.padEnd(100, '.');
-console.log(newString);
+let newString = 'loading'.padEnd(10, '.');
+//console.log(newString);
+
+const obj = {
+	name : "nitya",
+	get foo() {
+		return 25;
+	}
+}
+
+//console.log(Object.getOwnPropertyDescriptors(obj));
+
+function add(a, b,) {
+	return a + b;
+}
+
+//console.log(add(140, 43));
+
+function loadExternalContent() {    
+	return new Promise((resolve, reject) => 
+		{        
+			setTimeout(() => {            
+				resolve('hello');        
+			}, 3000);    
+		}
+	);
+}
+
+async function getContent() {
+    const text = await loadExternalContent();    
+    console.log(text);
+}
+
+console.log('it will call function');
+getContent();
+console.log('it called function');
