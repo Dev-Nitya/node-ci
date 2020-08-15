28cece7d744ce36c1cace2f6b1d80baa4e4f1f99
niya <nitya.devstory@gmail.com>
2020-08-15 17:58:11 +0530

    es8 new features -- string

--- es8-try.js
+++ es8-try.js
@@ -5,9 +5,12 @@ const names = {
 };
 
 let namesArr = Object.values(names);
-console.log(namesArr);
+//console.log(namesArr);
 
 let entriesArr = Object.entries(names);
-console.log(entriesArr);
+//console.log(entriesArr);
 
 //console.log(Object.values(names));
+
+let newString = 'loading'.padEnd(100, '.');
+console.log(newString);
