2ce49d02732e84db769b7ccf48c5a728dad943df
niya <nitya.devstory@gmail.com>
2020-08-20 11:07:54 +0530

    node event loop --should continue return

--- loop.js
+++ loop.js
@@ -1,9 +1,14 @@
 //node myfile.js
 
+const pendingTimers = [], pendingOSTasks = [], pendingOperations = [];
+
 myfile.runContents();
 
 function shouldContinue(){
-
+	//Check one : Any pending setTimeout, setImmediate, setInterval?
+	//Check two : Any pending operating system tasks? (Like maybe a server listening to some given port)
+	//Check three : Any pending long running operations? (Like fs module)
+	return pendingTimers.length || pendingOSTasks.length || pendingOperations.length;
 }
 
 //Entire body executes in one 'tick'
