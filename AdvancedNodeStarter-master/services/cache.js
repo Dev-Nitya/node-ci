af6c2cab391e0544659dfb7e7370897c2b158dd7
niya <nitya.devstory@gmail.com>
2020-09-04 16:06:27 +0530

    redis custom

--- AdvancedNodeStarter-master/services/cache.js
+++ AdvancedNodeStarter-master/services/cache.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-	console.log("I'm about to run a query");
-
-	const key = Object.assign({}, this.getQuery(), {
+mongoose.Query.prototype.exec = async function() {
+	const key = JSON.stringify(Object.assign({}, this.getQuery(), {
 		collection : this.mongooseCollection.name
-	});
-	console.log(key);
+	}));
+	
+	//see if we have a value for key in redis
+	const cacheValue = await client.get(key);
 
-	return exec.apply(this, arguments);
+	//if we do, return that
+	if(cacheValue){
+		const doc = JSON.parse(cacheValue);
+
+		return Array.isArray(doc) 
+			? doc.map(d => new this.model(d))
+			: new this.model(doc); 
+	}
+
+	//issue the query, store the result in redis
+
+	const result  = await exec.apply(this, arguments);
+
+	client.set(key, JSON.stringify(result));
+
+	return result;
 }
