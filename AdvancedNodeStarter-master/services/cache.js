bee460ada1fcedefcb603bcd885160e74c01ff69
niya <nitya.devstory@gmail.com>
2020-09-04 15:41:47 +0530

    redis key custom

--- AdvancedNodeStarter-master/services/cache.js
+++ AdvancedNodeStarter-master/services/cache.js
@@ -5,7 +5,10 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
 	console.log("I'm about to run a query");
 
-	console.log(this.getQuery());
-	console.log(this.mongooseCollection.name);
+	const key = Object.assign({}, this.getQuery(), {
+		collection : this.mongooseCollection.name
+	});
+	console.log(key);
+
 	return exec.apply(this, arguments);
 }
