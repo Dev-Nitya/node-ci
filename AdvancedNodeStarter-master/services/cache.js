4d3b68095a4dbdacfe93d53e99c7b698585b8755
niya <nitya.devstory@gmail.com>
2020-09-04 16:16:08 +0530

    redis -- pipeline operation

--- AdvancedNodeStarter-master/services/cache.js
+++ AdvancedNodeStarter-master/services/cache.js
@@ -7,7 +7,17 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = async function() {
+	this.useCache = true;
+	return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
+
+	if(!this.useCache) {
+		return exec.apply(this, arguments);
+	}
+
 	const key = JSON.stringify(Object.assign({}, this.getQuery(), {
 		collection : this.mongooseCollection.name
 	}));
@@ -31,4 +41,4 @@ mongoose.Query.prototype.exec = async function() {
 	client.set(key, JSON.stringify(result));
 
 	return result;
-}
+};
