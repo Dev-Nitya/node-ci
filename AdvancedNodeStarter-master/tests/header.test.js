54ad6bbf9cd2b785dfeca65070526a495147eab8
niya <nitya.devstory@gmail.com>
2020-09-05 23:15:40 +0530

    jest factories -- session factory

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -30,21 +30,6 @@ test('clicking login starts Oauth flow', async () => {
 test('when signed in, shows logout button', async () => {
 	const id = '5f40f49c15d4743498245c8f';
 
-	const Buffer = require('safe-buffer').Buffer;
-	const sessionObject = {
-		passport : {
-			user : id 
-		}
-	};
-	const sessionString = Buffer.from(
-		JSON.stringify(sessionObject)
-		).toString('base64');
-
-	const Keygrip = require('keygrip');
-	const keys = require('../config/keys');
-	const keygrip = new Keygrip([keys.cookieKey]);
-	const sig = keygrip.sign('session=' + sessionString);
-	
 	await page.setCookie({ name : 'session', value : sessionString });
 	await page.setCookie({ name : 'session.sig', value : sig });
 
