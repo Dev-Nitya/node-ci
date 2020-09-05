9790920eef71f13c34e212130782b023ba2dbfe8
niya <nitya.devstory@gmail.com>
2020-09-05 15:44:53 +0530

    adding puppeteer

---
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+test('Add two numbers', () => {
+	const sum = 1 + 2;
+
+	expect(sum).toEqual(3); 
+});
+
+test('We can launch a browser', async () => {
+	const browser = await puppeteer.launch({
+		headless : false //headless mode means browser will open without GUI
+	});
+	const page = await browser.newPage();
+});
