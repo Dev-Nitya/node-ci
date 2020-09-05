f3d42b2beaaf7d33fe1535acbb3e1a866bdd35af
niya <nitya.devstory@gmail.com>
2020-09-05 16:14:41 +0530

    before each

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -1,15 +1,17 @@
 const puppeteer = require('puppeteer');
 
-test('Add two numbers', () => {
-	const sum = 1 + 2;
+let browser, page;
 
-	expect(sum).toEqual(3); 
+beforeEach(async () => { //this will run before every test
+	browser = await puppeteer.launch({
+		headless : false //headless mode means browser will open without GUI
+	});
+	page = await browser.newPage();
+	await page.goto('localhost:3000');
 });
 
 test('We can launch a browser', async () => {
-	const browser = await puppeteer.launch({
-		headless : false //headless mode means browser will open without GUI
-	});
-	const page = await browser.newPage();
-	await page.goto('localhost:3000');
+	const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+	expect(text).toEqual('Blogster');
 });
