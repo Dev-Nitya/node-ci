fdd492ae97f111808adabfbdf2e0d7d729861991
niya <nitya.devstory@gmail.com>
2020-09-05 16:32:39 +0530

    new test -- check oauth url

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -10,8 +10,19 @@ beforeEach(async () => { //this will run before every test
 	await page.goto('localhost:3000');
 });
 
-test('We can launch a browser', async () => {
+afterEach(async () => {
+	await browser.close();
+});
+
+test('the header has the correct text', async () => {
 	const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
 	expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts Oauth flow', async () => {
+	await page.click('.right a');
+
+	const url = await page.url();
+	expect(url).toMatch('/accounts\.google\.com/');
 });
