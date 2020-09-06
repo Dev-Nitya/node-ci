3390f608c696a2c91285b7a45e7a2533984ec111
niya <nitya.devstory@gmail.com>
2020-09-06 14:41:20 +0530

    refactoring usig Es6 proxy

--- AdvancedNodeStarter-master/tests/header.test.js
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -1,19 +1,19 @@
 const puppeteer = require('puppeteer');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+
+
+let page;
 
 beforeEach(async () => { //this will run before every test
-	browser = await puppeteer.launch({
-		headless : false //headless mode means browser will open without GUI
-	});
-	page = await browser.newPage();
+	page = await Page.build();
 	await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-	await browser.close();
+	await page.close();
 });
 
 test('the header has the correct text', async () => {
